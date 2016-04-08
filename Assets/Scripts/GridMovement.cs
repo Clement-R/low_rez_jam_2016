cdbc7c62abf4ed7a788b73efd83013e9b61e8cdd
Clement-R <rondeau.clement@gmail.com>
2016-04-08 22:54:29 +0200

    Add movement and camera following

---
+++ Assets/Scripts/GridMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridMovement : MonoBehaviour {
+    Vector3 pos;
+    public float speed = 3.0f;
+
+    void Start() {
+        pos = transform.position;
+    }
+
+    void FixedUpdate() {
+        if (Input.GetKey(KeyCode.Q) && transform.position == pos) {
+            pos += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D) && transform.position == pos) {
+            pos += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.Z) && transform.position == pos) {
+            pos += Vector3.up;
+        }
+        if (Input.GetKey(KeyCode.S) && transform.position == pos) {
+            pos += Vector3.down;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+    }
+}
--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -2,14 +2,56 @@
 using System.Collections;
 
 public class WorldGenerator : MonoBehaviour {
+    public GameObject[] floorTiles;
+    public int[][] level;
+    
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Use this for initialization
+    void Start () {
+        level = new int[][] {
+            new int[] { 1, 1, 1, 1, 1, 1, 1, 1 },
+            new int[] { 1, 1, 1, 1, 1, 1, 1, 1 },
+            new int[] { 1, 1, 1, 1, 1, 1, 1, 1 },
+            new int[] { 1, 1, 1, 1, 1, 1, 1, 1 },
+            new int[] { 1, 1, 1, 1, 1, 1, 1, 1 },
+            new int[] { 1, 1, 1, 1, 1, 1, 1, 1 },
+            new int[] { 1, 1, 1, 1, 1, 1, 1, 1 },
+            new int[] { 1, 1, 1, 1, 1, 1, 1, 1 }
+        };
+
+        this.createLevel();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void createLevel () {
+        for (int i = 0; i < level.Length; i++) {
+            int[] row = level[i];
+
+            for (int j = 0; j < row.Length; j++) {
+                int tile = row[j];
+
+                GameObject toInstantiate;
+
+                switch (tile) {
+                    case 1:
+                        toInstantiate = floorTiles[0] as GameObject;
+                        break;
+                    default:
+                        toInstantiate = null;
+                        break;
+                }
+
+                float x = j - 3.5f;
+                float y = (level.Length - 1) - i - 3.5f;
+
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+                // instance.transform.SetParent(boardHolder);
+                // Quaternion.AngleAxis(90.0f, new Vector3(0, 0, 1));
+            }
+        }
+    }
 }
