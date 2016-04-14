b261a6a8561134f47086ae7976ef03302e44f521
Scarounet <rondeau.clement@gmail.com>
2016-04-14 08:55:22 +0200

    Add bomb prefab with animation and movement behavior

---
+++ Assets/Scripts/BombBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombBehavior : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.down * Time.deltaTime * speed);
+	}
+}
