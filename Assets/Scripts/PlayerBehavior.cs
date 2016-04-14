cec85f92a93144b1b19832d318ac76d94e2e3588
Scarounet <rondeau.clement@gmail.com>
2016-04-14 18:07:15 +0200

    Add collision detection between player and bombs

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour {
+	BoxCollider2D playerCollider;
+
+	// Use this for initialization
+	void Start () {
+		playerCollider = this.GetComponent<BoxCollider2D> ();
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		// Debug.Log("You loose !");
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log("You loose !");
+	}
+}
