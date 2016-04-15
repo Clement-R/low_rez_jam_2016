3fb908085a220b8c1b7659e9fe081045cd6fba07
Scarounet <rondeau.clement@gmail.com>
2016-04-15 16:02:53 +0200

    Change player movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,29 +2,30 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-	public float speed = 1.0f;
+	public float speed = 0.5f;
+	private Rigidbody2D rb2d;
+
 	private Animator anim;
 	private bool isRunning = false;
 
-	// Use this for initialization
 	void Awake () {
+		rb2d = GetComponent<Rigidbody2D> ();
 		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		
+		float xPos = transform.position.x + (Input.GetAxis("Horizontal") * speed);
+		float yPos = transform.position.y + (Input.GetAxis("Vertical") * speed);
+		transform.position = new Vector3 (Mathf.Clamp (xPos, -3.5f, 3.5f), Mathf.Clamp (yPos, -3.5f, 3.5f), 0f);;
 
-		/*
-		if (transform.position != pos) {
+		if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
 			isRunning = true;
 		} else {
 			isRunning = false;
 		}
 
 		anim.SetBool("isRunning", isRunning);
-
-		transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
-		*/
 	}
 }
